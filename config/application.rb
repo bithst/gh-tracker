7d8c21fba0cc70719a18343b9bbdc913736a6b5c
Bishal Thapa Shrestha <bishalthapashrestha@gmail.com>
2023-01-09 09:00:42 +0545

    feat: add service folder in autoload paths

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,6 @@ module GhTracker
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.autoload_paths += %W[#{config.root}/services/*]
   end
 end
